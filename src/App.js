23b424ef881cf1a40c6e78d20af8c74a6fc17fae
Yuan Zhou <yzhou@windsormachine.com>
2021-04-05 09:56:54 -0400

    Layout with ant design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const {Header, Sider, Content} = Layout;
+
 
 function App() {
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggle = () =>
+    setCollapsed(collapsed=>!collapsed);
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<VideoCameraOutlined />}>
+            Event
+          </Menu.Item>
+          <Menu.Item key="2" icon={<UserOutlined />}>
+            People
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            Profile
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{padding:0}}>
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 1000,
+          }}>
+            Content
+          </Content>
+      </Layout>
+    </Layout>
   );
 }
 
